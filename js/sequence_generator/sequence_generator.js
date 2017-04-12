cc36edcf881aafae6b2502022c1ded63e6d1095d
Christopher Dinh <cqdinh153@gmail.com>
2017-04-12 15:42:18 -0400

    Update sequence_generator.js

--- js/sequence_generator/sequence_generator.js
+++ js/sequence_generator/sequence_generator.js
@@ -57,11 +57,11 @@ function lstm(x, h, c, lstm, forget_bias=1.0){
     var f = [];
     var o = [];
     var out_size = b.length / 4;
-    console.log(input.toString());
     for(var row_num = 0; row_num < out_size; row_num++){
         i.push(b[row_num]);
         j.push(b[row_num]);
         f.push(b[row_num] + forget_bias);
+        console.log(f[0].toString());
         o.push(b[row_num]);
         for(var n = 0; n < input.length; n++){
             i[i.length-1] += input[n] * w[row_num + out_size * 0][n];
